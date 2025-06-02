0a21129787b001e0da193cab1bc4b32773f52678
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2025-06-02 19:21:50 -0400

    Slight optimizations

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HolonomicDriveOpMode.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/*
+ * This OpMode contains code for driving a 4-motor Omni-Directional (or Holonomic) robot.
+ * This code will work with either a Mecanum-Drive or an X-Drive train.
+ * Note that a Mecanum drive must display an X roller-pattern when viewed from above.
+ * Also note that it is critical to set the correct rotation direction for each motor.
+ *
+ * Holonomic drives provide the ability for the robot to move in three axes (directions) simultaneously.
+ * Each motion axis is controlled by one Joystick axis.
+ */
+
+@TeleOp(name = "Holonomic Drive OpMode", group = "Linear OpMode")
+public class HolonomicDriveOpMode extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        // Initialize the hardware
+        // The strings here must correspond to the names of the motors in your robot config
+        DcMotor leftFrontDrive = hardwareMap.dcMotor.get("left_front");
+        DcMotor leftBackDrive = hardwareMap.dcMotor.get("left_back");
+        DcMotor rightFrontDrive = hardwareMap.dcMotor.get("right_front");
+        DcMotor rightBackDrive = hardwareMap.dcMotor.get("right_back");
+
+        // Set motor directions
+        // You may need to adjust these for your particular robot
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Any code before this point runs once ONCE when you press INIT
+        waitForStart();
+
+        // Any code after this point runs ONCE after you press START
+
+        // Any code in this loop runs REPEATEDLY until the driver presses STOP
+        while (opModeIsActive()) {
+            double max;
+
+            // Left joystick to go forward & strafe, and right joystick to rotate.
+            double axial = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+            double lateral = gamepad1.left_stick_x;
+            double yaw = gamepad1.right_stick_x;
+
+            // Combine the joystick requests for each axis-motion to determine each wheel's power.
+            // Set up a variable for each drive wheel to save the power level for telemetry.
+            double leftFrontPower = axial + lateral + yaw;
+            double rightFrontPower = axial - lateral - yaw;
+            double leftBackPower = axial - lateral + yaw;
+            double rightBackPower = axial + lateral - yaw;
+
+            // Normalize the values so no wheel power exceeds 100%
+            // This ensures that the robot maintains the desired motion.
+            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+            max = Math.max(max, Math.abs(leftBackPower));
+            max = Math.max(max, Math.abs(rightBackPower));
+
+            if (max > 1.0) {
+                leftFrontPower /= max;
+                rightFrontPower /= max;
+                leftBackPower /= max;
+                rightBackPower /= max;
+            }
+
+            // Send calculated power to wheels
+            leftFrontDrive.setPower(leftFrontPower);
+            rightFrontDrive.setPower(rightFrontPower);
+            leftBackDrive.setPower(leftBackPower);
+            rightBackDrive.setPower(rightBackPower);
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
+            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
+            telemetry.update();
+        }
+        // Any code after the while loop will run ONCE after the driver presses STOP
+    }
+}
