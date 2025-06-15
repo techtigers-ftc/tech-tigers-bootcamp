218ef8b728ba6f1984ce29bc5a852c3910ab0f26
sidr2011 <159734650+sidr2011@users.noreply.github.com>
2025-06-15 16:58:18 -0400

    Code Works

--- TeamCode/src/main/java/org/firstinspires/ftc/CameraSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/CameraSubsystem.java
@@ -1,24 +1,20 @@
 package org.firstinspires.ftc;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-
 import android.util.Size;
 
-import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.opencv.core.Scalar;
 
-public class CameraSubsystem extends SubsystemBase {
+public class CameraSubsystem {
 
     private Scalar lowerBound = new Scalar(90, 150, 20); // Lower HSV bound
     private Scalar upperBound = new Scalar(140, 255, 255); // Upper HSV bound
     private Size cameraResolution = new Size(640, 480);
     private final VisionPortal visionPortal;// Camera resolution
     private final ColorFilterPipeline pipeline;
-    private boolean objectDetected = false; // Flag to indicate object detection
 
     public CameraSubsystem(HardwareMap hardwareMap) {
 
@@ -39,11 +35,7 @@ public class CameraSubsystem extends SubsystemBase {
     }
 
     public boolean isObjectDetected() {
-        return objectDetected;
+        return pipeline.isObjectDetected();
     }
 
-    @Override
-    public void periodic() {
-        objectDetected = pipeline.isObjectDetected();
-    }
 }
