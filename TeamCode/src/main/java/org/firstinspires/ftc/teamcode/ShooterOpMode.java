c7a58cb70a59442bdb24271e9282aa1001af6d6d
Josh Raikman <joshraikman@gmail.com>
2025-09-12 08:16:14 -0400

    updated else-if statement to make more sense and be more fluid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterOpMode.java
@@ -32,38 +32,34 @@ public class ShooterOpMode extends LinearOpMode {
         while (opModeIsActive()) {
             if (robotStatus.equals("READY")) {
                 // The robot is ready. Check for button presses to start shooting or grip the ball
-                if (gamepad1.a) {
-                    timer.reset(); // Reset the timer when starting to drive
-                    robotStatus = "DRIVING"; // Mark that we are now driving
-                } else if(gamepad1.b) {
+                if (gamepad1.b) {
                     manipulatorSubsystem.sweeperLeft(); // Move the sweeper to the left
+                    timer.reset(); // Reset the timer to track how long the servo has been moving for
                     robotStatus = "GRIPPING"; // Mark that we are now gripping
                 } else if (gamepad1.x) {
                     manipulatorSubsystem.sweeperRight(); // Move the sweeper to the right
+                    timer.reset(); // Reset the timer to track how long the servo has been moving for
                     robotStatus = "GRIPPING"; // Mark that we are now gripping
                 }
+            } else if (robotStatus.equals("GRIPPING")) {
+                if (timer.seconds() > 0.2) {
+                    // It has been enough time since we started gripping
+                    if (gamepad1.a) {
+                        timer.reset(); // Reset the timer when starting to drive
+                        driveSubsystem.drive(0.5, 0, 0); // Start driving forward at half speed
+                        robotStatus = "DRIVING"; // Mark that we are now driving
+                    }
+                }
             } else if (robotStatus.equals("DRIVING")) {
-                // Check if 0.5 seconds have passed
+                // Check if 0.5 seconds have passed, otherwise do nothing as you continue driving
                 if (timer.seconds() > 0.5) {
-                    // It has been 0.5 seconds, so stop driving and release the ball
                     driveSubsystem.drive(0, 0, 0); // Stop driving
                     manipulatorSubsystem.resetSweeper(); // Reset the sweeper position
                     timer.reset(); // Reset the timer for the shooting phase
                     robotStatus = "SHOOTING"; // Mark that we are now shooting
-                } else {
-                    // The robot is currently driving.
-                    driveSubsystem.drive(0.5, 0, 0);
                 }
-            } else if(robotStatus.equals("SHOOTING")) {
-                // It typically takes some time for the servo to complete its movement. We use a
-                // timer to wait for the servo to finish moving.
-                if (timer.seconds() > 0.2) {
-                    // It has been enough time since we started shooting, so reset the sweeper
-                    robotStatus = "READY"; // Mark that we are now stopped
-                }
-            } else {
-                // It typically takes some time for the servo to complete its movement. We use a
-                // timer to wait for the servo to finish moving.
+            } else if (robotStatus.equals("SHOOTING")) {
+                // Before we do anything else, we need to wait for the shooting to finish
                 if (timer.seconds() > 0.2) {
                     // It has been enough time since we started shooting, so reset the sweeper
                     robotStatus = "READY"; // Mark that we are now stopped
